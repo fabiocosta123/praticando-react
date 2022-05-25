38ee2d2ada8512e08e02e0b22d801d74a4107db8
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-05-24 23:44:43 -0300

    treinando conceito de listas

---
+++ src/components/OutraLista.js
@@ -0,0 +1,12 @@
+function OutraLista({ itens }) {
+  return (
+    <>
+      <h3>Listas de coisas boas</h3>
+      {itens.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
+    </>
+  );
+}
+
+export default OutraLista;
